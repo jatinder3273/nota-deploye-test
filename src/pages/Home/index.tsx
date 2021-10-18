f271969f2785167b84510cef6516bb1c91bfbec6
Pradeep <pradeep.wadhwa14@gmail.com>
2021-10-18 19:04:18 +0530

    Chages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,9 +3,7 @@ import TopBanner from "./Sections/TopBanner";
 import imageOne from "./../../assets/images/image1.png";
 import { ReactComponent as NextArrow } from "./../../assets/images/next-arrow.svg";
 import { Link } from "react-router-dom";
-import DarkMode from "../../hooks/context/DarkModeContext";
 const Home: React.FunctionComponent = () => {
-  const darkMode = React.useContext(DarkMode);
 
   return (
     <>
